2eafbffdeb671b14b35817ff062741aabbd8d573
ZZJ <zhaozjseu@163.com>
2020-06-06 18:43:20 +0800

    2020年完成

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const vm = new Vue({
   el: '#app',
   data() {
     return {
+      // 原始信息
       original: [
         { id: 1, name: '徐潇航', avatar: './avatars/xxh.jpg' },
         { id: 2, name: '郝世杰', avatar: './avatars/hsj.jpg' },
@@ -21,53 +22,91 @@ const vm = new Vue({
       ].map(k => Object.assign(k, {
         total: 0,
         normalTotal: 0,
-        specialTotal: 0
+        specialTotal: 0,
       })),
+      // 投票结果
+      candidateRes: null,
       candidates: null,
       pendingCandidates: [],
-      duration: 15000,
+      duration: 1500,
       shown: false,
       animating: false
     }
   },
   async created() {
-    this.candidates = this.original
-    document.body.ondragleave = e => e.preventDefault()
-    document.body.ondrop = async e => {
-      e.stopPropagation()
-      e.preventDefault()
-      let file = e.dataTransfer.files[0]
-      let reader = new FileReader()
-      let skeleton = JSON.parse(JSON.stringify(this.original))
-      reader.onload = e => {
-        let data = e.target.result
-        let workbook = XLSX.read(data, { type: 'binary' })
-
-        let csv = XLSX.utils.sheet_to_csv(workbook.Sheets.Sheet1)
-        csv = csv.split('\n').filter(k => /^.+(,.+){4}$/.test(k)).slice(1).map(k => {
-          let [ id, name, total, specialTotal, normalTotal ] = k.split(',')
-          id = parseInt(id)
-          total = parseFloat(total)
-          normalTotal = parseFloat(normalTotal)
-          specialTotal = parseFloat(specialTotal)
-          return { id, name, total, specialTotal, normalTotal }
-        }).sort((a, b) => a.id - b.id).slice(0, 20).map((k, i) => Object.assign(skeleton[i], k))
-        
-        this.load({ candidate: csv })
+   
+    // 一个深拷贝
+    this.candidateRes = this.deepClone(this.original)
+    // 获取投票结果
+    let res = await axios.get('https://xgbxscwx.seu.edu.cn/api/yxl/vote-result')
+    .then(
+      ret =>{
+        return ret.data.success ? ret.data.result : []
       }
-      reader.readAsBinaryString(file)
-    }
-    document.body.ondragenter = e => e.preventDefault()
-    document.body.ondragover = e => e.preventDefault()
+    )
+    res.forEach((item) => {
+      let candidateID = + Object.keys(item)[0]
+      this.candidateRes[candidateID - 1].total = +item[candidateID]
+      this.candidateRes[candidateID - 1].normalTotal = +item[candidateID]
+    })
+    console.log('load')
+
+    this.candidates = this.original
+    
+    // 监听键盘输入
+    document.addEventListener('keydown', e => {
+      // 展示投票结果
+      if(e.key === 'Enter'){
+        if(!this.shown && !this.animating){
+          console.log('show')
+          this.load({ candidate: this.candidateRes })
+          this.show()
+        }
+      }
+      // 清空投票结果
+      if(e.key === 'c'){
+        console.log('clear')
+        location.reload()
+      }
+    })
+    // 拖入文件加载票数
+    // document.body.ondragleave = e => e.preventDefault()
+    // document.body.ondrop = async e => {
+    //   e.stopPropagation()
+    //   e.preventDefault()
+    //   let file = e.dataTransfer.files[0]
+    //   let reader = new FileReader()
+    //   let skeleton = JSON.parse(JSON.stringify(this.original))
+    //   reader.onload = e => {
+    //     let data = e.target.result
+    //     let workbook = XLSX.read(data, { type: 'binary' })
+
+    //     let csv = XLSX.utils.sheet_to_csv(workbook.Sheets.Sheet1)
+    //     csv = csv.split('\n').filter(k => /^.+(,.+){4}$/.test(k)).slice(1).map(k => {
+    //       let [ id, name, total, specialTotal, normalTotal ] = k.split(',')
+    //       id = parseInt(id)
+    //       total = parseFloat(total)
+    //       normalTotal = parseFloat(normalTotal)
+    //       specialTotal = parseFloat(specialTotal)
+    //       return { id, name, total, specialTotal, normalTotal }
+    //     }).sort((a, b) => a.id - b.id).slice(0, 20).map((k, i) => Object.assign(skeleton[i], k))
+        
+    //     this.load({ candidate: csv })
+    //   }
+    //   reader.readAsBinaryString(file)
+    // }
+    // document.body.ondragenter = e => e.preventDefault()
+    // document.body.ondragover = e => e.preventDefault()
   },
   computed: {
     maxTotal() {
+      // 时时计算最大投票数
       return this.candidates.map(k => Number(k.total)).reduce((a, b) => a > b ? a : b, 1)
     }
   },
   methods: {
     async load(data) {
-      console.log('load')
+      // console.log('load')
       let { candidate } = data
       this.pendingCandidates = candidate
     },
@@ -75,7 +114,7 @@ const vm = new Vue({
       if (this.shown || this.animating) {
         return
       }
-      console.log('show')
+      // console.log('show')
       this.animating = true
       this.candidates = this.pendingCandidates
       setTimeout(() => {
@@ -84,6 +123,22 @@ const vm = new Vue({
         this.shown = true
         this.animating = false
       }, this.duration)
-    }
+    },
+    // 工具函数
+    deepClone(obj){
+      let objClone = Array.isArray(obj)?[]:{};
+      if(obj && typeof obj==="object"){
+        for(key in obj){
+          if(obj.hasOwnProperty(key)){
+            if(obj[key]&&typeof obj[key] ==="object"){
+              objClone[key] = this.deepClone(obj[key]);
+            }else{
+              objClone[key] = obj[key];
+            }
+          }
+        }
+      }
+      return objClone;
+    }    
   }
 })
